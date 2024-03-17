1743656b87443a9a99c13de594ab2f928ca7fc4b
michaelhope <hopemichael720@gmail.com>
2024-03-17 23:15:13 +0000

    Add grpc server files

---
+++ grpc_server.py
@@ -0,0 +1,46 @@
+import csv
+import grpc
+import logging
+import math
+
+from concurrent import futures
+
+import meterusage_pb2
+import meterusage_pb2_grpc
+
+logger = logging.getLogger(__name__)
+
+
+class MeterUsageService(meterusage_pb2_grpc.MeterUsageServiceServicer):
+    def GetMeterUsage(self, request, context):
+        """
+        Opens and serves the data in the csv file as defined in MeterUsageResponse in the proto file
+        :return:
+        """
+        records = []
+
+        try:
+            with open('data/meterusage.csv', mode='r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    # handle NaN records by filling in with 0.0
+                    if math.isnan(float(row["meterusage"])):
+                        records.append(meterusage_pb2.MeterUsageRecord(time=row["time"], meterusage=0.0))
+                    else:
+                        records.append(
+                            meterusage_pb2.MeterUsageRecord(time=row["time"], meterusage=float(row["meterusage"]))
+                        )
+        except Exception as exc:
+            logger.exception(f"Error processing file or file contents; msg={exc}")
+
+        return meterusage_pb2.MeterUsageResponse(records=records)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    meterusage_pb2_grpc.add_MeterUsageServiceServicer_to_server(MeterUsageService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()

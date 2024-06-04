f1f2a50c74f91c8770344302a89ec6b67053eff8
mhope <hopemichael720@gmail.com>
2024-06-04 18:25:38 +0000

    updated Dockerfile.app and Dockerfile.grpc

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
+
 import grpc
 import meterusage_pb2
 import meterusage_pb2_grpc
 
+from dotenv import load_dotenv
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
+load_dotenv()
 
 app = FastAPI(title="an HTTP server that serves data from a gRPC server as JSON")
 
@@ -17,8 +21,7 @@ async def read_meter_usage():
     Returns the data served by the gRPC server as JSON
     :return:
     """
-    # TODO: move to env variable
-    with grpc.insecure_channel("grpc_server:50051") as channel:
+    with grpc.insecure_channel(os.getenv("SERVER_ADDRESS")) as channel:
         stub = meterusage_pb2_grpc.MeterUsageServiceStub(channel)
         response = stub.GetMeterUsage(meterusage_pb2.MeterUsageRequest())
 
